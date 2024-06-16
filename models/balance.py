12019972f879679ce33193f1ac238d29a4fda817
Luis Pereira <luispereira.tkd@gmail.com>
2024-06-16 22:56:41 +0100

    models: isolation of balance model

---
+++ models/balance.py
@@ -0,0 +1,34 @@
+import datetime as dt
+from typing import Union
+
+
+class Balance:
+    def __init__(
+        self,
+        description: str,
+        value: float,
+        credit: bool,
+        bank_id: int,
+        registered_at: dt.datetime,
+        balance: Union[float, None] = None,
+    ):
+        self.description = description
+        self.value = value
+        self.credit = credit
+        self.balance = balance
+        self.bank_id = bank_id
+        self.registered_at = registered_at
+
+    def generate_create_data(self):
+        return f"{self.registered_at.isoformat()};{self.description};{self.credit};{self.value};{self.balance}"
+
+    def to_dict(self):
+        return {
+            "date": self.registered_at.isoformat(),
+            "description": self.description,
+            "value": self.value,
+            "credit": self.credit,
+            "balance": self.balance,
+            "bank_id": self.bank_id,
+            "createData": self.generate_create_data(),
+        }
--- models/coverflex.py
+++ models/coverflex.py
@@ -1,35 +1,8 @@
 import os
 import json
 import datetime as dt
-from typing import Dict, List, Union
-
-
-class Balance:
-    def __init__(
-        self,
-        description: str,
-        value: float,
-        credit: bool,
-        bank_id: int,
-        registered_at: dt.datetime,
-        balance: Union[float, None] = None,
-    ):
-        self.description = description
-        self.value = value
-        self.credit = credit
-        self.balance = balance
-        self.bank_id = bank_id
-        self.registered_at = registered_at
-
-    def to_dict(self):
-        return {
-            "description": self.description,
-            "value": self.value,
-            "credit": self.credit,
-            "balance": self.balance,
-            "bank_id": self.bank_id,
-            "date": self.registered_at.isoformat(),
-        }
+from typing import Dict, List
+from models.balance import Balance
 
 
 class CoverflexBalance:
@@ -42,6 +15,25 @@ class CoverflexBalance:
 
     def parse_datetime(self, balance_datetime: str) -> dt.datetime:
         return dt.datetime.strptime(balance_datetime, "%Y-%m-%dT%H:%M:%S.%fZ")
+
+    def validate_fields(self, movement) -> bool:
+
+        if "description" not in movement:
+            return False
+
+        if "is_debit" not in movement:
+            return False
+
+        if "executed_at" not in movement:
+            return False
+
+        if "amount" not in movement or "amount" not in movement["amount"]:
+            return False
+
+        if "balance_after" not in movement or "amount" not in movement["balance_after"]:
+            return False
+
+        return True
 
     def parse_json_file(self) -> List[Dict]:
         if not os.path.isfile(self.filepath):
@@ -62,7 +54,9 @@ class CoverflexBalance:
         self.movements.clear()
 
         for movement in movement_list:
-            # TODO: validate field existence
+            if not self.validate_fields(movement):
+                # TODO: log error
+                continue
 
             balance = Balance(
                 description=movement["description"],
