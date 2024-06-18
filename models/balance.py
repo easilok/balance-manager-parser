9a720798796112ceaf12a7159931eaaf1b841dc1
Luis Pereira <luispereira.tkd@gmail.com>
2024-06-18 21:26:14 +0100

    balance: fix date format for api expected format

--- models/balance.py
+++ models/balance.py
@@ -18,13 +18,14 @@ class Balance:
         self.balance = balance
         self.bank_id = bank_id
         self.registered_at = registered_at
+        self.date_format = "%Y-%m-%d"
 
-    def generate_create_data(self):
-        return f"{self.registered_at.isoformat()};{self.description};{self.credit};{self.value};{self.balance}"
+    def generate_create_data(self) -> str:
+        return f"{self.registered_at.strftime(self.date_format)};{self.description};{self.credit};{self.value};{self.balance}"
 
     def to_dict(self):
         return {
-            "date": self.registered_at.isoformat(),
+            "date": self.registered_at.strftime(self.date_format),
             "description": self.description,
             "value": self.value,
             "credit": self.credit,
